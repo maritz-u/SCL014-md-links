425bcc5ba068f5e5b065d94d62b711e0303f14de
Maritzu Zúñiga <maritzudelosangeles@gmail.com>
2020-11-15 22:44:21 -0300

    Crea esbozos de test

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,27 @@
-const mdLinks = require('../');
+const mdLinks = require("../index");
 
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe("mdLinks", () => {
+  it("registrar todos los enlaces", () => {
+    mdLinks("./some/example.md")
+      .then((links) => {
+        // => [{ href, text, file }]
+      })
+      .catch(console.error);
   });
 
+  it("validar los archivos si la flag validate es true", () => {
+    mdLinks("./some/example.md", { validate: true })
+      .then((links) => {
+        // => [{ href, text, file, status, ok }]
+      })
+      .catch(console.error);
+  });
+
+  it("registrar todos los enlaces de los ficheros MD en una carpeta", () => {
+    mdLinks("./some/dir")
+      .then((links) => {
+        // => [{ href, text, file }]
+      })
+      .catch(console.error);
+  });
 });
