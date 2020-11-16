b8abcdfcce6904a3c396b9d127f90f759010284f
Maritzu Zúñiga <maritzudelosangeles@gmail.com>
2020-11-15 22:44:49 -0300

    Agrega funciones para procesar archivo único

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-module.exports = () => {
-  // ...
+const path = require("path");
+const fs = require("fs");
+const regexp = /\[([\w\s\d\.]+)\]\(([\w\d\.\/:]+)\)/g;
+
+const procesarPath = (path) => {
+  if (!fs.existsSync(path)) {
+    console.error("No se encuentra!");
+    return;
+  }
+
+  const stats = fs.lstatSync(path);
+
+  if(stats.isDirectory()) {
+
+  } else if (stats.isFile()) {
+    leerArchivo(path);
+  }
+
+  console.error("Solo se reconocen directorios o archivos")
+  return;
+};
+
+const leerArchivo = (path) => {
+  fs.readFile(path, "utf8", (err, data) => procesarTexto(data, path));
+}
+
+const procesarTexto = (data, path) => {
+    let coincidencia;
+    const resultados = [];
+    while ((coincidencia = regexp.exec(data)) !== null) {
+      resultados.push({
+        href: coincidencia[2],
+        text: coincidencia[1],
+        file: path,
+      });
+    }
+    console.log(resultados);
+};
+
+procesarPath(process.argv[2]);
+
+exports.modules = {
+  mdLinks: (path, opts) =>
+    new Promise((resolve, reject) => {
+      resolve(llamadoDeRuta(procesarPath(path)));
+    }),
 };
